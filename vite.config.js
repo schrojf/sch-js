5b4ca3a84770ae6740cea6745d3507dd4b6aaa9d
Viliam Schrojf <viliam@schrojf.sk>
2024-01-17 22:50:01 +0100

    Introduce `vitest` to project

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
+/// <reference types="vitest" />
+
+// Configure Vitest (https://vitest.dev/config/)
+
 /** @type {import('vite').UserConfig} */
 export default {
     base: '/sch-js/',
+    test: {
+        /* for example, use global to avoid globals imports (describe, test, expect): */
+        // globals: true,
+    },
 };
